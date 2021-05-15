75db886631294dd2a4bd0c8cc61485cfd4b82810
Tyler Brown <tyler.s.brown.93@gmail.com>
2021-05-14 21:22:05 -0700

    Fix timestamp bug

--- server/time.js
+++ server/time.js
@@ -2,13 +2,13 @@ var getCurrentTimestamp = () => {
   var date = new Date();
 
   var y = date.getFullYear();
-  var m = date.getMonth().toString().length === 1 ? `0${date.getMonth()}` : date.getMonth();
+  var m = date.getMonth().toString().length === 1 ? `0${date.getMonth() + 1}` : date.getMonth();
   var d = date.getDate().toString().length === 1 ? `0${date.getDate()}` : date.getDate();
   var h = date.getHours().toString().length === 1 ? `0${date.getHours()}` : date.getHours();
   var min = date.getMinutes().toString().length === 1 ? `0${date.getMinutes()}` : date.getMinutes();
   var s = date.getSeconds().toString().length === 1 ? `0${date.getSeconds()}` : date.getSeconds();
 
-  var currentTimestamp = `${y}-${m + 1}-${d} ${h}:${min}:${s}`;
+  var currentTimestamp = `${y}-${m}-${d} ${h}:${min}:${s}`;
 
   return currentTimestamp;
 }
